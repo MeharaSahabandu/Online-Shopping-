ea9f8e6f94611a5240dd56ba9c3c8f9405c726ee
Mehara Sahabandu <119718801+MeharaSahabandu@users.noreply.github.com>
2023-02-16 21:54:21 +0530

    User.js model added

---
+++ BACKEND/models/User.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const schema = mongoose.schema;
+
+const userSchema = new schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+});
+
+module.exports = User = mongoose.model('user',userSchema);
