00a797881a34e10a25c456bdb3f77541a5fd3329
Mehara Sahabandu <119718801+MeharaSahabandu@users.noreply.github.com>
2023-02-16 21:55:22 +0530

    edited

--- BACKEND/models/User.js
+++ BACKEND/models/User.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const schema = mongoose.schema;
 
 const userSchema = new schema({
-    name: { type: String, required: true },
+    //name: { type: String, required: true },
     email: { type: String, required: true },
     password: { type: String, required: true },
 });
